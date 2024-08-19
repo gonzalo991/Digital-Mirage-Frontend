1e0ae0a6c72d506573f3c70be223e81255d8059a
gonzalo <gonxaraya991@gmail.com>
2024-08-19 20:51:17 -0300

    cambios en imagenes

--- src/components/Fragments/Banner.jsx
+++ src/components/Fragments/Banner.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import banner from '../../Img/bannercm.png';
+import banner from '../..images/bannercm.png';
 
 const Banner = () => {
     return (
-        <section className=" ">
-            <div>
-                <img
-                    src={banner}
-                    alt="imagen"
-                    className="mt-20 br h-auto px-4 py-8 sm:pxlg:px-8 lg:py-16 hidden sm:block"
-                />
-            </div>
-        </section>
+        <>
+            <section className="">
+                <div>
+                    <img
+                        src={banner}
+                        alt="imagen"
+                        className="mt-20 br h-auto px-4 py-8 sm:pxlg:px-8 lg:py-16 hidden sm:block"
+                    />
+                </div>
+            </section>
+        </>
     )
 }
 
