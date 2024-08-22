1253e3f68ec86858dc289529ab6533b278dc3658
gonzalo <gonxaraya991@gmail.com>
2024-08-22 14:04:56 -0300

    cambios minimos

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://gonzalo991.github.io/Digital-Mirage-Frontend/",
+  base: "https://gonzalo991.github.io/",
 })
