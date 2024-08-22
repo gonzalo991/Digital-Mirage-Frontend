402982457c25bccf81fb69cbbf84f131d45e7973
gonzalo <gonxaraya991@gmail.com>
2024-08-22 18:01:31 -0300

    cambio en la base de viteconf

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "https://gonzalo991.github.io/Digital-Mirage-Frontend/",
 })
