28f59c36bc9eea9e6be1e9f19007bebb9dae791b
gonzalo <gonxaraya991@gmail.com>
2024-08-22 17:51:40 -0300

    cambios en el filter, centrado de opciones

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://gonzalo991.github.io/",
+  base: "./",
 })
